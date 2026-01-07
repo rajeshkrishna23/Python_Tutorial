3251a490e9b13bc643919057d1cef0ef576a68ae
rajeshkrishna23 <43347583+rajeshkrishna23@users.noreply.github.com>
2026-01-07 19:54:10 +0530

    Delete my_Program.py

--- my_Program.py
+++
@@ -1,4 +0,0 @@
-firstNumber = int(input('Enter first number: '))
-secondNumber = int(input('Enter second number: '))
-result = firstNumber + secondNumber
-print(f"The addition of {firstNumber} and {secondNumber} is :{result} ")
